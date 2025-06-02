7a08d6068077d8533efdf91cef0303c8c8e5c09d
ike-agu <ikennaww@yahoo.fr>
2025-06-02 18:00:46 +0100

    added comments for time-format assignment

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -11,15 +11,17 @@ console.log(result);
 
 // For the piece of code above, read the code and then answer the following questions
 
-// a) How many variable declarations are there in this program?
+// a) How many variable declarations are there in this program? 6
 
-// b) How many function calls are there?
+// b) How many function calls are there? None
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+// It represents the division remainder of 8784 seconds and 60 seconds, which is 24.
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
-
-// e) What do you think the variable result represents? Can you think of a better name for this variable?
+// 8784 - 24 seconds / 60 = 146 minutes
+// e) What do you think the variable result represents? Can you think of a better name for this variable? The variable result holds a string interpolation representing the movie length in Hours,minutes and seconds.
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+// Yes!, It will work for all values inserted in movieLength because the variable results is using string interpolation to represent the output of movie length.
