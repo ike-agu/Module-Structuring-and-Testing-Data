2cb3fd1c5856686c74ee5b3fcd85d14f80a8fa7c
ike-agu <ikennaww@yahoo.fr>
2025-06-10 17:42:16 +0100

    Fixed the error in exercise 1-key-errors/1.js and Added a comment to expalain why the error was occuring

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,4 +1,5 @@
 // Predict and explain first...
+/*The function below is trying to convert a decimal number to percentage */
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
@@ -15,6 +16,23 @@ function convertToPercentage(decimalNumber) {
 console.log(decimalNumber);
 
 // =============> write your explanation here
+/*
+- The function convertToPercentage() takes a parameter called decimalNumber
+- it's declaring a constant variable with the parameter decimalNumber which will throw an error because it's withing the same scope and in JS is not allowed. Variable names suppose to be unique within the same scope.
+- It's storing a hardcoded value of 0.5 as value to the constant variable decimalNumber
+- It creates a constant variable percentage to store the value of multiplying decimalNumber by 100 to get the percentage
+- It returns percentage
+- Finally, it logs the parameter decimalNumber without the function which will generate an error.
+*/
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function convertToPercentage(decimalNumber) {
+  const decimalNumberValue = decimalNumber;
+  const percentage = `${decimalNumberValue * 100}%`;
+
+  return percentage;
+}
+
+console.log(convertToPercentage(0.8));
