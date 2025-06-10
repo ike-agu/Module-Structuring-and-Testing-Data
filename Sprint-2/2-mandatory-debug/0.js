a858622b2eddb10fdd569e3425119d3ffa64d5ea
ike-agu <ikennaww@yahoo.fr>
2025-06-10 18:15:32 +0100

    Fixed the error in  2-mandatory-debug/0.js and added a comment to explain the error

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,23 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+/*
+- The function multiply takes 2 parameters a and b and logging the value of a multiplied by b within the function but it's not returning the value.
+
+*/
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
+
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// =============> write your explanation here
+// - Line 9 will log the value to the console but line 12 will generate undefined because the value of a * b is not returned.
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 
 function multiply(a, b) {
-  console.log(a * b);
+  return (a * b);
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
