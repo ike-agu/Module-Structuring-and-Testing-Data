3fe535b6759c2a66a073870956f99288f568b5db
ike-agu <ikennaww@yahoo.fr>
2025-06-24 18:10:46 +0100

    added codes from getCardValue function

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,15 @@
 function getCardValue(card) {
     // replace with your code from key-implement
-    return 11;
+    let rank = card.slice(0, -1); // this line removes the emojis
+
+    if (rank === "A") {
+      return 11;
+    } else if (+rank >= 2 && +rank <= 10) {
+      return +rank;
+    } else if (rank === "J" || rank === "Q" || rank === "K") {
+      return 10;
+    } else {
+      return "Invalid Card rank.";
+    }
 }
 module.exports = getCardValue;
