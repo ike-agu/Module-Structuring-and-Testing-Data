65fb3838357c84141e4542cf59ef8f99ec102362
ike-agu <ikennaww@yahoo.fr>
2025-06-24 20:05:19 +0100

    added tests for getOrdinalNumber from 1 - 11

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -10,4 +10,43 @@ const getOrdinalNumber = require("./get-ordinal-number");
 
 test("should return '1st' for 1", () => {
     expect(getOrdinalNumber(1)).toEqual("1st");
-    });
+ });
+
+test("Should return '2nd' for 2",()=>{
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+})
+
+test("Should return '3rd' for 3",()=>{
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+})
+
+test("Should return '4th' for 4",()=>{
+  expect(getOrdinalNumber(4)).toEqual("4th");
+})
+
+test("Should return '5th' for 5",()=>{
+  expect(getOrdinalNumber(5)).toEqual("5th");
+})
+
+test("Should return '6th' for 6",()=>{
+  expect(getOrdinalNumber(6)).toEqual("6th");
+})
+test("Should return '7th' for 7",()=>{
+  expect(getOrdinalNumber(7)).toEqual("7th");
+})
+
+test("Should return '8th' for 8",()=>{
+  expect(getOrdinalNumber(8)).toEqual("8th");
+})
+
+test("Should return '9th' for 9",()=>{
+  expect(getOrdinalNumber(9)).toEqual("9th");
+})
+
+test("Should return '10th' for 10",()=>{
+  expect(getOrdinalNumber(10)).toEqual("10th");
+})
+
+test("Should return '11th' for 11",()=>{
+  expect(getOrdinalNumber(11)).toEqual("11th");
+})
