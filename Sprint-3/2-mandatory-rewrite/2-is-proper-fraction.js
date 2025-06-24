2f478fdd6249b98e51386ca6237604822838c9a3
ike-agu <ikennaww@yahoo.fr>
2025-06-24 15:51:32 +0100

    added my isProperFraction function from key-implement

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,8 @@
 function isProperFraction(numerator, denominator) {
     if (numerator < denominator) return true;
     // add your completed function from key-implement here
+    if (numerator < denominator) return true;
+    if (numerator >= denominator) return false;
 }
 
 module.exports = isProperFraction;
