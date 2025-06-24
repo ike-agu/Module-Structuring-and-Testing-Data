5b28aa1efee1d1b6f6017ba60a16cd0286c8b1c9
ike-agu <ikennaww@yahoo.fr>
2025-06-24 18:48:03 +0100

    added tests for get card value exercise

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -1,11 +1,23 @@
 const getCardValue = require("./3-get-card-value");
 
 test("should return 11 for Ace of Spades", () => {
-    const aceofSpades = getCardValue("A♠");
-    expect(aceofSpades).toEqual(11);
+    expect(getCardValue("A♠")).toEqual(11);
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("Should return rank number between 2 and 10",()=>{
+  expect(getCardValue("5♥")).toEqual(5);
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("Should return 10 for Face Cards J, Q, K", () => {
+expect(getCardValue("K♠")).toEqual(10);
+expect(getCardValue("J♣")).toEqual(10);
+expect(getCardValue("Q♦")).toEqual(10);
+});
+
 // Case 4: Handle Ace (A):
 // Case 5: Handle Invalid Cards:
+test("should throw an error indicating Invalid card rank.", () => {
+  expect(getCardValue("")).toEqual("Invalid Card rank.");
+});
