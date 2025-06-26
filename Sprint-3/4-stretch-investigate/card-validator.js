0071b2fc20b7d6d21368af29ea45b4bc13c8b4b7
ike-agu <ikennaww@yahoo.fr>
2025-06-26 02:45:20 +0100

    Implemented credit card validator function

---
+++ Sprint-3/4-stretch-investigate/card-validator.js
@@ -0,0 +1,33 @@
+
+function creditCardValidator(cardNumber) {
+  let cardNumberArray = cardNumber.split(""); // convert to array of strings
+  let intNumber = cardNumberArray.map((num) => +num); // convert array of strings to array integers
+  let sum = 0;
+  let regexNum = /^[0-9]+$/;
+
+  if (!regexNum.test(cardNumber)) return "Invalid Characters."; //check if cardNumber contains any letter or special character
+
+  for (let i = 0; i < intNumber.length; i++) {
+    sum += intNumber[i];
+  }
+
+  if (cardNumber[cardNumber.length - 1] % 2 == 1)
+    return "Invalid card: odd final number";
+
+  if (sum < 16) return "Sum less than 16 ";
+
+  if (cardNumber.length === 16) return "Valid card";
+
+  // for(let i = 0; i < cardNumber.length; i++){
+  //     if(cardNumber[i] === cardNumber[1]) return "Invalid card: Only one type of number provided";
+  // }
+
+  return sum;
+}
+
+console.log(creditCardValidator("9999777788880000"));//valid card number
+console.log(creditCardValidator("6666666666661666"));//Valid card number
+console.log(creditCardValidator("a92332119c011112"));//invalid characters
+console.log(creditCardValidator("1111111111111110"));//sum less than 16
+console.log(creditCardValidator("4444444444444444")); //should fail bc Only one type of number provided"
+console.log(creditCardValidator("6666666666666661"));//odd final number
