9e62f363e20a830dd48839f1d3987fe518b51369
ike-agu <ikennaww@yahoo.fr>
2025-07-04 00:16:49 +0100

    Implemented get get ordinal number function

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,22 @@
 function getOrdinalNumber(num) {
- return "1st";
+  let lastNumber = num % 10; // Using the modulo operator to get the last digit of the number.
+  let lastTwoNumbers = num % 100; //Using the modulo operator to get the last two digits of the number.
+
+  if (lastTwoNumbers === 11 || lastTwoNumbers === 12 || lastTwoNumbers === 13) {
+    return num + "th";
+  }
+
+  if (lastNumber === 1) {
+    return num + "st";
+  } else if (lastNumber === 2) {
+    return num + "nd";
+  } else if (lastNumber === 3) {
+    return num + "rd";
+  } else {
+    return num + "th";
+  }
 }
+
+console.log(getOrdinalNumber(3));
 
 module.exports = getOrdinalNumber;
