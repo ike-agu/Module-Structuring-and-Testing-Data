8d57ee1000c02913ea003ff9cafb47fc8b0da4a6
ike-agu <ikennaww@yahoo.fr>
2025-07-03 19:10:44 +0100

    implemented passwordValidator function

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,6 +1,29 @@
 function passwordValidator(password) {
-    return password.length < 5 ? false : true
+  const passwords = ["PasswordAccess1!", "PasswordAccess2!", "PasswordAccess3!"];
+  if (password.length < 5) {
+    return false;
+  }
+  if (passwords.includes(password)) {
+    return `${password} already used.`;
+  }
+  if (!/[a-z]/.test(password)) {
+    return false;
+  }
+  if (!/[A-Z]/.test(password)) {
+    return false;
+  }
+  if (!/[0-9]/.test(password)) {
+    return false;
+  }
+  if (!/[!#$%\.\*&]/.test(password)) {
+    return false;
+  }
+
+  return true;
 }
 
+console.log(passwordValidator("Password1!"));
+console.log(passwordValidator("P2!4"));
+console.log(passwordValidator("PasswordAccess1!"));
 
 module.exports = passwordValidator;
