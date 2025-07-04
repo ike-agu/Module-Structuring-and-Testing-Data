79fcbe921641d0c4e86ef0e41338dc858f74d638
ike-agu <ikennaww@yahoo.fr>
2025-07-04 02:18:23 +0100

    Implemented repeat function

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,10 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str, count) {
+  if (count < 0) {
+    return "Negative numbers are not valid!";
+  } else {
+    return str.repeat(count);
+  }
 }
 
+console.log(repeat("hello", 10));
 module.exports = repeat;
