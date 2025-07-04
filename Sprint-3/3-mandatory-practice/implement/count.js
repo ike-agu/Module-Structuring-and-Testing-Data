1e0ea6cc46b111c863ff96a1cb2fcc849140b876
ike-agu <ikennaww@yahoo.fr>
2025-07-04 01:57:17 +0100

    Implemented count Character function

--- Sprint-3/3-mandatory-practice/implement/count.js
+++ Sprint-3/3-mandatory-practice/implement/count.js
@@ -1,5 +1,12 @@
-function countChar(stringOfCharacters, findCharacter) {
-    return 5
+function countChar(strOfCahracters, findcharacter) {
+  let count = 0;
+  for (let i = 0; i < strOfCahracters.length; i++) {
+    if (strOfCahracters[i] === findcharacter) {
+      count++;
+    }
+  }
+  return count;
 }
 
+// console.log(countChar("aaaaa", "a"));
 module.exports = countChar;
