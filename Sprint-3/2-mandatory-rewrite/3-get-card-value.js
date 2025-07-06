155ed1e813d6b74f65396aee03599321a9823359
ike-agu <ikennaww@yahoo.fr>
2025-07-06 22:46:19 +0100

    updated get-card-value.js to ensure only standard card ranks are accepted.

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -2,14 +2,17 @@ function getCardValue(card) {
     // replace with your code from key-implement
     let rank = card.slice(0, -1); // this line removes the emojis
 
+    const rangeTwoToTenArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];//
+
     if (rank === "A") {
       return 11;
-    } else if (+rank >= 2 && +rank <= 10) {
-      return +rank;
+    } else if (rangeTwoToTenArray.includes(rank)) {
+      return parseInt(rank);
     } else if (rank === "J" || rank === "Q" || rank === "K") {
       return 10;
     } else {
       return "Invalid Card rank.";
     }
 }
+
 module.exports = getCardValue;
